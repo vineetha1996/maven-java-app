e5939e0958b40a5f5b2bbeeb15e9fef0e8bed804
Vineetha <vineethay96@gmail.com>
2021-02-19 15:55:51 -0600

    Added project files

---
+++ src/main/java/edu/nwmissouri/bigdata/vineetha/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.vineetha;
+
+/**
+ * This begins execution of my app
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
